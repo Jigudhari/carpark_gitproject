b3aae96fcec6fe9c7101a41ddc1c98a6260b1cf9
20012424 <20012424@tafe.wa.edu.au>
2024-11-26 05:50:51 +0800

    Add  class with default parameter values and initialization logic

--- src/car_park.py
+++ src/car_park.py
@@ -1,3 +1,23 @@
 class CarPark:
-    pass
+    def __init__(self,
+                 location="Unknown",
+                 capacity=0,
+                 plates=None,
+                 sensors=None,
+                 displays=None
+                 ):
+        if plates is None:
+            plates = []
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates or []  # List of current license plates in the car park
+        self.sensors = sensors or []  # List of Sensor objects
+        self.displays = displays or []  # List of Display objects
 
+    def __str__(self):
+        """Return a string representation of the CarPark object."""
+        return f"CarPark at {self.location} with {self.capacity} bays."
+
+
+if __name__ == '__main__':
+    print(repr(CarPark()))
