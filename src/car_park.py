6c19467992bb4a4dc5a60621d939eaaddf00e8b1
20012424 <20012424@tafe.wa.edu.au>
2024-11-28 11:48:50 +0800

    A CarPark class -methods update-logging car activity

--- src/car_park.py
+++ src/car_park.py
@@ -1,5 +1,7 @@
 from sensor import Sensor
 from display import Display
+from pathlib import Path
+from datetime import datetime # we'll use this to timestamp entries
 
 
 class CarPark:
@@ -8,13 +10,17 @@ class CarPark:
                  capacity=0,
                  plates=None,
                  sensors=None,
-                 displays=None
+                 displays=None,
+                 log_file=Path("log.txt")
                  ):
         self.location = location
         self.capacity = capacity
         self.plates = plates or []  # List of current license plates in the car park
         self.sensors = sensors or []  # List of Sensor objects
         self.displays = displays or []  # List of Display objects
+        self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
+        # create the file if it doesn't exist:
+        self.log_file.touch(exist_ok=True)
 
     @property
     def available_bays(self):
@@ -50,11 +56,17 @@ class CarPark:
     def add_car(self, plate):
         self.plates.append(plate)
         self.update_displays()
+        self._log_car_activity(plate, "entered")
 
     def remove_car(self, plate):
         self.plates.remove(plate)
         self.update_displays()
+        self._log_car_activity(plate, "exited")
 
+    # in CarPark class
+    def _log_car_activity(self, plate, action):
+        with self.log_file.open("a") as f:
+            f.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
     def __str__(self):
         """Return a string representation of the CarPark object."""
         return f"CarPark at {self.location} with {self.capacity} bays."
